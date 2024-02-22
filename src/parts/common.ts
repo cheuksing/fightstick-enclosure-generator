0322a40fee8f468c1d6c38c8e75ec3b2f7d918f5
tommy <starchip009988@hotmail.com>
2024-02-22 20:52:44 +0800

    Remove dead code and improve performance

--- src/parts/common.ts
+++ src/parts/common.ts
@@ -1,14 +1,34 @@
-import {model, models, chain} from 'makerjs';
+import {model, models, chain, type IModel} from 'makerjs';
 import {filletG2Continunity} from '@helpers/fillet';
 import {getConfig} from '@config';
 
+type FilletedResult = {
+  straightLines: IModel;
+  filletArc: IModel;
+};
+
+const cache = new Map<string, FilletedResult>();
+
+function getFilletResultWithCache(width: number, height: number, borderRadius: number) {
+  const key = `${width}-${height}-${borderRadius}`;
+  const cached = cache.get(key);
+
+  if (cached) {
+    return cached;
+  }
+
+  const dimension = new models.Rectangle(width, height);
+  const c = model.findSingleChain(dimension);
+  const filletArc = filletG2Continunity(c, borderRadius, 0.5);
+
+  const result = {straightLines: chain.toNewModel(c), filletArc};
+
+  cache.set(key, result);
+
+  return result;
+}
+
 export function common() {
   const config = getConfig();
-  const dimension = new models.Rectangle(config.width, config.height);
-  const c = model.findSingleChain(dimension);
-  const filletArc = filletG2Continunity(c, config.borderRadius, 0.5);
-
-  return {
-    models: {straightLines: chain.toNewModel(c), filletArc},
-  };
+  return getFilletResultWithCache(config.width, config.height, config.borderRadius);
 }
